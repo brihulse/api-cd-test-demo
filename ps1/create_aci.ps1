f55ae496b7b05e74ee5dd40515a37ceb057d0dc4
Brian Hulse <bhulse@rightpoint.com>
2019-10-13 22:47:21 -0500

    push everything

--- ps1/create_aci.ps1
+++
@@ -1,3 +0,0 @@
-az --version
-
-az container create --resource-group todo-api-demo-rg --name demo-container --image docker.io/brihulse/rp-todo-demo --dns-name-label rp-todo-demo --ports 80 --location centralus

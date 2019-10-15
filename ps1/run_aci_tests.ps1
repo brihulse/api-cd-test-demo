bca9342628f92425247935a788de05b1d753351f
Brian Hulse <bhulse@rightpoint.com>
2019-10-14 23:17:36 -0500

    drop delete

--- ps1/run_aci_tests.ps1
+++ ps1/run_aci_tests.ps1
@@ -1,7 +1,7 @@
+az container stop --resource-group todo-api-demo-rg --name demo-container-tester
+
 az container create --resource-group todo-api-demo-rg --name demo-container-tester --image docker.io/brihulse/rp-todo-demo-postman-runner --location centralus
 
 az container start --resource-group todo-api-demo-rg --name demo-container-tester
 
 az container logs --resource-group todo-api-demo-rg --name demo-container-tester
-
-az container delete --resource-group todo-api-demo-rg --name demo-container-tester

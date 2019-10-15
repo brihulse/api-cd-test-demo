0a00ac3636f87a2af57dab020b783245851b9b8e
Brian Hulse <bhulse@rightpoint.com>
2019-10-14 20:16:36 -0500

    adjust newman docker and scripts

--- ps1/run_aci_tests.ps1
+++ ps1/run_aci_tests.ps1
@@ -1,5 +1,3 @@
-az --version
-
 az container create --resource-group todo-api-demo-rg --name demo-container-tester --image docker.io/brihulse/rp-todo-demo-postman-runner --restart-policy Never --location centralus
 
-az container attach --resource-group todo-api-demo-rg --name demo-container-tester
+az container exec -g MyResourceGroup --name demo-container-tester --container-name demo-container-tester --exec-command "run TodoDemoIntegrationTests.postman_collection.json -e TodoTest-Integration.postman_environment.json -r cli"

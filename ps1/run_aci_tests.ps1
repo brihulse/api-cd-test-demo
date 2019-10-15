c176043d400b32f700f07a25ced8a597dc9213f0
Brian Hulse <bhulse@rightpoint.com>
2019-10-14 20:54:57 -0500

    fixes to scripts

--- ps1/run_aci_tests.ps1
+++ ps1/run_aci_tests.ps1
@@ -1,3 +1,3 @@
-az container create --resource-group todo-api-demo-rg --name demo-container-tester --image docker.io/brihulse/rp-todo-demo-postman-runner --restart-policy Never --location centralus
+az container create --resource-group todo-api-demo-rg --name demo-container-tester --image docker.io/brihulse/rp-todo-demo-postman-runner --location centralus
 
-az container exec -g todo-api-demo-rg --name demo-container-tester --container-name demo-container-tester --exec-command "run TodoDemoIntegrationTests.postman_collection.json -e TodoTest-Integration.postman_environment.json -r cli"
+#az container exec -g todo-api-demo-rg --name demo-container-tester --container-name demo-container-tester --exec-command "run TodoDemoIntegrationTests.postman_collection.json -e TodoTest-Integration.postman_environment.json -r cli"

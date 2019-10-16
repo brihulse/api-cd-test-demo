874a16de6d865a25742abb9fbbbdd7efe71a7edb
Brian Hulse <bhulse@rightpoint.com>
2019-10-16 12:34:46 -0500

    add png for markdown. get rid of stop command

--- ps1/run_aci_tests.ps1
+++ ps1/run_aci_tests.ps1
@@ -1,5 +1,3 @@
-az container stop --resource-group todo-api-demo-rg --name demo-container-tester
-
 az container create --resource-group todo-api-demo-rg --name demo-container-tester --image docker.io/brihulse/rp-todo-demo-postman-runner --location centralus
 
 az container start --resource-group todo-api-demo-rg --name demo-container-tester

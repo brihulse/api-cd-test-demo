ecb7658d445d779efeca5ff2df54e8e61666e45f
Brian Hulse <bhulse@rightpoint.com>
2019-11-09 17:06:33 -0600

    update pipeline, update postman src

--- ps1/teardown.ps1
+++ ps1/teardown.ps1
@@ -1,5 +1,5 @@
-az container stop --resource-group todo-api-demo-rg --name $env:TESTCONTAINERNAME
+#az container stop --resource-group todo-api-demo-rg --name $env:TESTCONTAINERNAME
 az container stop --resource-group todo-api-demo-rg --name $env:APICONTAINERNAME
 
 az container delete --resource-group todo-api-demo-rg --name $env:APICONTAINERNAME -y
-az container delete --resource-group todo-api-demo-rg --name $env:TESTCONTAINERNAME -y
+#az container delete --resource-group todo-api-demo-rg --name $env:TESTCONTAINERNAME -y
